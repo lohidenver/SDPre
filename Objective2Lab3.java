9973f39880d5aae73dae8d162815e943bbd9484f
Dave Roberts <56813289+lohidenver@users.noreply.github.com>
2021-07-03 00:17:47 -0600

    Add files via upload

---
+++ Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 20.0;
+    num2 = 25.0;
+    num3 = 54.0;
+    average = (num1+num2+num3)/3;
+    System.out.println(average);
+  }
+}
