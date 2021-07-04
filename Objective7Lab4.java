15044eb321e3657e9b9606a147936da16f6e787a
Dave Roberts <56813289+lohidenver@users.noreply.github.com>
2021-07-04 13:49:33 -0600

    Add files via upload

---
+++ Objective7Lab4.java
@@ -0,0 +1,11 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 1;
+    int sum = 0;
+    while (counter<=20) {
+      sum = counter + sum;
+      counter++;
+    }
+    System.out.println(sum);
+  }
+}
