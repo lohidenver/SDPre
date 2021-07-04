426952d56a92a03184e61cb4b02c709787bdd176
Dave Roberts <56813289+lohidenver@users.noreply.github.com>
2021-07-03 18:18:30 -0600

    Add files via upload

---
+++ Objective3Lab2.java
@@ -0,0 +1,7 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+      double num1 = 7, num2 = 10, result;
+      result = Math.sqrt((num1 * num1) + (num2 * num2));
+      System.out.println("The hypontenuse of a triangle with sides " + num1 + " and " + num2 + " is " + result + ".");
+  }
+}
