4f54165d5ed2ec7f67a09bdd7da5a1a485faa557
Dave Roberts <56813289+lohidenver@users.noreply.github.com>
2021-07-04 16:56:30 -0600

    Add files via upload

---
+++ Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    System.out.println("");
+    for (int i=0; i<=10; i++){
+      System.out.println(i);
+      }
+    System.out.println("");//TODO Write a for loop that will print the numbers 1-10
+    for (int i=10; i>=0; i--){
+      System.out.println(i);
+      }
+    //TODO Write another for loop that will print the numbers 10-1.
+  }
+}
---
+++ Objective8Lab3.java
@@ -0,0 +1,19 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+    int counterlimit = 20;
+    for (int i=0; i< counterlimit; i++) {
+      if (counter%2 == 0){
+        System.out.println(counter + " is even.");
+        }
+      else {
+        System.out.println(counter + " is odd.");
+        }
+        counter++;
+      }
+
+    /*
+      Your solution goes here
+    */
+  }
+}
