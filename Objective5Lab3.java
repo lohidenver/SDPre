7139340a431822192fd7d87b7d64549962878ace
Dave Roberts <56813289+lohidenver@users.noreply.github.com>
2021-07-04 11:20:06 -0600

    Add files via upload

---
+++ Objective5Lab3.java
@@ -0,0 +1,18 @@
+public class Objective5Lab3 {
+    public static void main(String[] args) {
+      java.util.Scanner scanner = new java.util.Scanner(System.in);
+      System.out.println("Please enter a number: ");
+      int userNum = scanner.nextInt();
+      if (userNum > 0){
+        System.out.println("Your number is positive.");
+      }
+      else if (userNum < 0){
+        System.out.println("Your number is negative.");
+      }
+      else {
+        System.out.println("Your number is neither positive or negative.");
+      }
+
+      scanner.close();
+  }
+}
